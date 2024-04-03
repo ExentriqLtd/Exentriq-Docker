ac709ee4327cf6f1a2c17a8a3afb5f6df035e8aa
zhe1ka <zheka.dzecina@gmail.com>
2024-04-03 16:30:53 +0300

    docker: added info to bash file

--- start.sh
+++ start.sh
@@ -46,3 +46,14 @@ meteor npm i
 cp ./run_template.sh ./run.sh
 cp ./settings-development.json ./settings-local.json
 sed -i '' "s|\"rootFolder\": \"\"|\"rootFolder\": \"$parent_dir\"|g" ./settings-local.json
+
+echo "\033[35m=============================================\033[0m"
+echo "\033[32m"; echo "The project is successfully prepared"; echo "\033[0m"
+
+echo "You also need to install and have running:"
+echo "- MongoDB (version 4.4)"
+echo "- Redis"
+
+echo ""
+echo "\033[35m=============================================\033[0m"
+echo ""
